b88b6680c3bea8ac0e83228aaa2f34622f8201a6
rupert <github@thesett.com>
2025-11-21 23:34:17 +0000

    Added test cases for compaction.

---
+++ test/CompactionTest.hpp
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "TestSuite.hpp"
+
+// Compaction tests
+extern Testing::Test testBlockInitialization;
+extern Testing::Test testBlockLiveInfoTracking;
+extern Testing::Test testCompactionSetSelection;
+extern Testing::Test testObjectEvacuationWithForwarding;
+extern Testing::Test testReadBarrierSelfHealing;
+extern Testing::Test testBlockEvacuation;
+extern Testing::Test testBlockReclaimToTLABs;
+extern Testing::Test testCompactionPreservesValues;
+extern Testing::Test testRootPointerUpdatesAfterCompaction;
+extern Testing::Test testFragmentationDefragmentation;
