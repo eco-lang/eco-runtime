653f4f6bc775b4763c3a4b4ae41b3db0660f5bb1
Rupert Smith <rupert@thesett.com>
2025-11-24 15:07:26 +0000

    Removed pointless nusey allocation retry when out of space, as this is already known from the previous check against the threshold max.

--- runtime/src/GarbageCollector.cpp
+++ runtime/src/GarbageCollector.cpp
@@ -216,43 +216,6 @@ void *GarbageCollector::allocate(size_t size, Tag tag) {
             }
             return obj;
         }
-
-        // Nursery full - trigger GC only if we haven't already.
-        if (!gc_triggered) {
-            minorGC();
-        }
-
-        // Try again after GC.
-        obj = nursery->allocate(size);
-        if (obj) {
-            Header *hdr = getHeader(obj);
-            std::memset(hdr, 0, sizeof(Header));
-            hdr->tag = tag;
-            switch (tag) {
-                case Tag_String:
-                    hdr->size = (size - sizeof(ElmString)) / sizeof(u16);
-                    break;
-                case Tag_Custom:
-                    hdr->size = (size - sizeof(Custom)) / sizeof(Unboxable);
-                    break;
-                case Tag_Record:
-                    hdr->size = (size - sizeof(Record)) / sizeof(Unboxable);
-                    break;
-                case Tag_DynRecord:
-                    hdr->size = (size - sizeof(DynRecord)) / sizeof(HPointer);
-                    break;
-                case Tag_FieldGroup:
-                    hdr->size = (size - sizeof(FieldGroup)) / sizeof(u32);
-                    break;
-                case Tag_Closure:
-                    hdr->size = (size - sizeof(Closure)) / sizeof(Unboxable);
-                    break;
-                default:
-                    hdr->size = size;
-                    break;
-            }
-            return obj;
-        }
     }
 
     // Allocate in old gen.
